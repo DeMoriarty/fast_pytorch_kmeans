a2056b9c7e47788c4752363078747cec2b6d0426
DeMoriarty <sahbanjan@gmail.com>
2023-03-15 20:28:26 +0300

    increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'fast_pytorch_kmeans',         # How you named your package folder (MyLib)
   packages = ['fast_pytorch_kmeans'],   # Chose the same as "name"
-  version = '0.1.8',      # Start with a small number and increase it with every change you make
+  version = '0.1.9',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'a fast kmeans clustering algorithm implemented in pytorch',   # Give a short description about your library
   author = 'demoriarty',                   # Type in your name
